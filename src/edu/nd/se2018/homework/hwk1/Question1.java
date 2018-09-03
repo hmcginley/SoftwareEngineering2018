e15b008e9a204e57638b362e0e492ed939e4572e
hmcginley <hmcginleynd@gmail.com>
2018-09-03 15:16:01 -0400

    Question 1

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,21 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.HashSet;
+import java.util.Vector;
+
 public class Question1 {
-		
-	public Question1(){}
-	
-	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
-	}
+        public Question1() {}
+
+        public int getSumWithoutDuplicates(int[] numbers){
+                int total = 0;
+                HashSet sumSet = new HashSet();
+                for (int j = 0; j < numbers.length; j++) {
+                        if (!sumSet.contains(numbers[j])) {
+                                total += numbers[j];
+                                sumSet.add(numbers[j]);
+                        }
+                }
+                return total;
+        }
 }
+
