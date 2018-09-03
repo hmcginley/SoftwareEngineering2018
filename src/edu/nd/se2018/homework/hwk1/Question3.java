62049bd853d70107c3cc42420f3f325fea93e9e9
hmcginley <hmcginleynd@gmail.com>
2018-09-03 15:17:46 -0400

    Question 3

--- src/edu/nd/se2018/homework/hwk1/Question3.java
+++ src/edu/nd/se2018/homework/hwk1/Question3.java
@@ -1,9 +1,36 @@
 package edu.nd.se2018.homework.hwk1;
 public class Question3 {
-	
-	public Question3(){}	
-	
+
+        public Question3(){}
+
     public int getMirrorCount(int[] numbers){
-		return 0;	
-	}
+        if (numbers.length == 0) {
+                return 0;
+        }
+
+                int maxMirror = 1;
+
+                for (int i = 0; i < numbers.length; i++) {
+                        int currMax = 0;
+                        for (int j = numbers.length - 1; j >=0; j--) {
+                                if (i + currMax < numbers.length) {
+                                        if (numbers[i + currMax] == numbers[j]) {
+                                                currMax++;
+                                        }
+                                        else {
+                                                if (currMax > maxMirror) {
+                                                        maxMirror = currMax;
+                                                }
+                                                currMax = 0;
+                                        }
+                                }
+                        }
+
+                        if (currMax > maxMirror) {
+                                maxMirror = currMax;
+                        }
+                }
+
+                return maxMirror;
+        }
 }
